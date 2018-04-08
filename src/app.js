7de7d49eeab92c85db4cd068913751f189713b31
Oussama Barkouki <oussama1598@gmail.com>
2018-04-08 15:02:41 -0700

    added commander to the app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log('Welcome')
+import program from 'commander';
+import { version } from '../package.json';
+
+program
+  .version(version)
+  .usage('[options] <name>')
+  .parse(process.argv);
+
+(async function main() {
+  if (!program.args.length) return program.outputHelp();
+
+  return null;
+})();
